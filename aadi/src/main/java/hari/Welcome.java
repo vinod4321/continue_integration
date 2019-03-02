72457d390d8955df731fca34d82202931d503030
vinod kumar <vinukumar831@gmail.com>
2019-03-02 02:39:45 -0800

    maven

---
+++ aadi/src/main/java/hari/Welcome.java
@@ -0,0 +1,13 @@
+package hari;
+
+//import org.eiichiro.acidhouse.Entity;
+//import org.eiichiro.acidhouse.Key;
+
+//@Entity
+public class Welcome {
+
+//	@Key String key;
+	
+	String status;
+	
+}
---
+++ aadi/src/main/java/hari/WelcomeRepository.java
@@ -0,0 +1,17 @@
+package hari;
+
+import org.eiichiro.gig.Repository;
+import org.eiichiro.jaguar.inject.Name;
+
+@Name("hari.WelcomeRepository")
+@Repository
+public class WelcomeRepository {
+
+	public Welcome get() {
+		// It's OK to get it from database.
+		Welcome welcome = new Welcome();
+		welcome.status = "READY";
+		return welcome;
+	}
+	
+}
